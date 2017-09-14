d904f9fd48d43bbad0f2aa19898f0622a4b3710a
易无解 <2198105373@qq.com>
2017-09-15 00:37:58 +0800

    test1

--- index.php
+++ index.php
@@ -12,8 +12,8 @@
     </tr> <tr>
         <td>
             <?php
-            require ('conn.php')
-            $result = mysqli_query("select * from bbs_info order by id DESC ");
+            require ('conn.php');
+            $result = mysqli_query("select * from bbs_info order by id DESC", MYSQLI_STORE_RESULT);
             while ($row = mysqli_fetch_array($result, MYSQLI_BOTH))
             {
             ?>
