d9a1b7a401a1c9d1d39e2e2f796ba002e0e1f2f0
易无解 <2198105373@qq.com>
2017-09-15 11:23:49 +0800

    modify data.php

--- data.php
+++ data.php
@@ -5,9 +5,9 @@
  * Date: 15/09/2017
  * Time: 11:08
  */
-$output = array();
+$json = array();
 $id = @$_GET['id'] ? $_GET['id'] : 0;
 if (empty($id)) {
-    $output = array('data' => NULL, 'info' => '无该id信息', 'code' => -201);
-    exit(json_decode($output));
+    $json = array('data' => NULL, 'info' => '无该id信息', 'code' => -201);
+    exit(json_encode($json));
 }
