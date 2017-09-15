f750f059d2f760d977c239faea15d01c06840f70
易无解 <2198105373@qq.com>
2017-09-15 11:45:16 +0800

    modify data.php

--- data.php
+++ data.php
@@ -5,9 +5,21 @@
  * Date: 15/09/2017
  * Time: 11:08
  */
+require ('conn.php');
 $json = array();
 $id = @$_GET['id'] ? $_GET['id'] : 0;
 if (empty($id)) {
     $json = array('data' => NULL, 'info' => '无该id信息', 'code' => -201);
     exit(json_encode($json));
 }
+$result = mysqli_query($conn, 'select * from bbs_info where id='.intval($id));
+$row = mysqli_fetch_row($result);
+if (!$row) {
+    $json = array('data' => NULL, 'info' => '无该id信息', 'code' => -201);
+    exit(json_encode($json));
+}
+while ($row = mysqli_fetch_array($result, MYSQLI_BOTH)) {
+    $json.addslashes(array('id' => $row['id'], 'name' => $row['name'], 'content' => $row['content'], 'time' => $row['time']));
+}
+exit(json_encode($json));
+mysqli_free_result($result);
